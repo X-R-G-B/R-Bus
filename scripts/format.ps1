54acbdfa81dd579eac19b3af00f97cc590a16028
Xavier Mitault <xavier.mitault@epitech.eu>
2023-09-23 13:30:40 +0200

    CICD: Fix search for file
    PATCH

--- scripts/format.ps1
+++ scripts/format.ps1
@@ -1,6 +1,6 @@
 #!powershell
 # Format c++ files and exit with error if files are not formatted
 
-$files = Get-Childitem –Path C:\ -Include *.cpp,*.hpp -Recurse -ErrorAction SilentlyContinue
+$files = Get-Childitem –Path src -Include *.cpp,*.hpp -Recurse -ErrorAction SilentlyContinue
 
 clang-format --style=file -i $files
--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Format c++ files and exit with error if files are not formatted
 
-files=$(find . -name '*.cpp' -o -name '*.hpp')
+files=$(find src/ -name '*.cpp' -o -name '*.hpp')
 
-clang-format --style=file -i 
+clang-format --style=file -i $files
