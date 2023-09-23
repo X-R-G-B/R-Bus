ec04f70fc466f870ee5498e61aa13c1a52a5d629
Xavier Mitault <xavier.mitault@epitech.eu>
2023-09-23 20:51:35 +0200

    CICD: Remove option for ubuntu compatibility

--- scripts/compil.ps1
+++ scripts/compil.ps1
@@ -7,7 +7,7 @@ cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 if ($args.Count -gt 0 -and $args[0] -eq '--dry-run') {
     $files = Get-Childitem -Path src -Include *.cpp -Recurse -ErrorAction SilentlyContinue
-    clang-tidy -format-style=file --fix --fix-notes -p build $files
+    clang-tidy -format-style=file --fix --fix-notes --fix-errors -p build $files
 }
 
 cmake --build build
