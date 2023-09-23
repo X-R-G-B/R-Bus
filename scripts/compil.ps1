e3e379d8018768b39f39b182a6e907f2df93987c
Xavier Mitault <xavier.mitault@epitech.eu>
2023-09-23 23:24:17 +0200

    CICD: Integrate clang tidy with cmake

--- scripts/compil.ps1
+++ scripts/compil.ps1
@@ -3,11 +3,4 @@
 
 $ErrorActionPreference = 'Stop'
 
-cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-
-if ($args.Count -gt 0 -and $args[0] -eq '--dry-run') {
-    $files = Get-Childitem -Path src -Include *.cpp -Recurse -ErrorAction SilentlyContinue
-    clang-tidy -format-style=file --fix --fix-notes --fix-errors -p build $files
-}
-
 cmake --build build
--- scripts/compil.sh
+++ scripts/compil.sh
@@ -3,12 +3,4 @@
 
 set -ex
 
-cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-
-if [[ "$1" == "--dry-run" ]]; then
-    find src/ \( -name '*.cpp' -o -name '*.hpp' \) -print0 \
-        | xargs -0 \
-            clang-tidy -format-style=file -p build --fix --fix-notes --fix-errors
-fi
-
-cmake --build build
+cmake --build build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
