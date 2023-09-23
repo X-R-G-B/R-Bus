0e76611f1f085cdf535163d0ad3d468222568122
Xavier Mitault <xavier.mitault@epitech.eu>
2023-09-23 13:47:01 +0200

    CICD: Add flag to exit with status code

--- scripts/format.ps1
+++ scripts/format.ps1
@@ -1,6 +1,13 @@
 #!powershell
 # Format c++ files and exit with error if files are not formatted
 
+$argClangFormat = ''
+
+# check if first argument is '--dry-run'
+if ($args.Count -gt 0 -and $args[0] -eq '--dry-run') {
+    $argClangFormat = '--dry-run --Werror'
+}
+
 $files = Get-Childitem –Path src -Include *.cpp,*.hpp -Recurse -ErrorAction SilentlyContinue
 
-clang-format --style=file -i $files
+clang-format --style=file $argClangFormat -i $files
--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Format c++ files and exit with error if files are not formatted
 
+ARGS=
+
+if [ "$1" == "--dry-run" ]; then
+    ARGS="--dry-run --Werror"
+fi
+
 files=$(find src/ -name '*.cpp' -o -name '*.hpp')
 
-clang-format --style=file -i $files
+clang-format --style=file $ARGS -i $files
