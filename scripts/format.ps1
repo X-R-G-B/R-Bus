50e3b577a617f27d45355df116ed1d91b0691730
Xavier Mitault <xavier.mitault@epitech.eu>
2023-09-23 14:24:17 +0200

    CICD: Fix ps1 list
    PATCH

--- scripts/format.ps1
+++ scripts/format.ps1
@@ -1,11 +1,10 @@
 #!powershell
 # Format c++ files and exit with error if files are not formatted
 
-$argClangFormat = ''
+$argClangFormat = @()
 
-# check if first argument is '--dry-run'
 if ($args.Count -gt 0 -and $args[0] -eq '--dry-run') {
-    $argClangFormat = '--dry-run --Werror'
+    $argClangFormat = @('--dry-run', '--Werror')
 }
 
 $files = Get-Childitem –Path src -Include *.cpp,*.hpp -Recurse -ErrorAction SilentlyContinue
