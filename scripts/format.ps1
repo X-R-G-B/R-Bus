101ce29a02970afffdbae2b062801259edd63cc9
Xavier Mitault <xavier.mitault@epitech.eu>
2023-09-23 15:38:24 +0200

    CICD: Fix ascii car encoding
    PATCH

--- scripts/format.ps1
+++ scripts/format.ps1
@@ -7,6 +7,6 @@ if ($args.Count -gt 0 -and $args[0] -eq '--dry-run') {
     $argClangFormat = @('--dry-run', '--Werror')
 }
 
-$files = Get-Childitem –Path src -Include *.cpp,*.hpp -Recurse -ErrorAction SilentlyContinue
+$files = Get-Childitem -Path src -Include *.cpp,*.hpp -Recurse -ErrorAction SilentlyContinue
 
 clang-format --style=file $argClangFormat -i $files
