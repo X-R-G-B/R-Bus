6d79d5d9296fda92e3ec23e7a3481cab87757ac3
Xavier Mitault <xavier.mitault@epitech.eu>
2023-09-25 01:35:14 +0200

    CICD: Fix release bad version numeric
    PATCH

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -3,7 +3,7 @@
 
 BRANCH="$1"
 
-LAST_TAG=$(git tag | tail -n 1)
+LAST_TAG=$(git tag | sort -t '.' --numeric-sort -k1,1 -k2,2 -k3,3 | tail -n 1)
 LAST_TAG_MAJOR=$(echo "$LAST_TAG" | cut -d'.' -f1)
 LAST_TAG_MINOR=$(echo "$LAST_TAG" | cut -d'.' -f2)
 LAST_TAG_PATCH=$(echo "$LAST_TAG" | cut -d'.' -f3)
