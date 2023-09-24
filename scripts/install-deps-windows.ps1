f21c1e964d89541fcf86bc186bb618d75d70b74d
Xavier Mitault <xavier.mitault@epitech.eu>
2023-09-24 23:09:24 +0200

    CICD: Fix windows install deps
    PATCH

--- scripts/install-deps-windows.ps1
+++ scripts/install-deps-windows.ps1
@@ -6,6 +6,6 @@ winget install --accept-package-agreements --accept-source-agreements CMake -e
 
 ./deps/vcpkg/bootstrap-vcpkg.bat
 
-./deps/vcpkg/vcpkg integrate install
+./deps/vcpkg/vcpkg.exe integrate install
 
 ./scripts/install-package.ps1
