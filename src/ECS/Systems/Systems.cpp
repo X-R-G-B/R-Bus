71c9419d8983224c0294f69b7bcb5c46c059c4d2
Tenshi <tenshii.tv@gmail.com>
2023-10-03 22:49:12 +0200

    GAME-LOGIC: Fix animation rect PATCH

--- src/ECS/Systems/Systems.cpp
+++ src/ECS/Systems/Systems.cpp
@@ -158,18 +158,15 @@ namespace Systems {
         // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
         Registry::getInstance().getComponents<Types::AnimRect>().insertBack({
             spriteRect,
-            {spriteRect, // move
-             {2, 51, 46, 47},
+            {{2, 51, 46, 47},
               {101, 2, 48, 47},
               {152, 2, 46, 47},
-              {201, 2, 46, 47}  },
-            {spriteRect, // attack
-             {2, 51, 46, 47},
+              {201, 2, 46, 47}},
+            {{2, 51, 46, 47},
               {101, 2, 48, 47},
               {152, 2, 46, 47},
-              {201, 2, 46, 47}  },
-            {spriteRect, // dead x y with height
-             {180, 140, 18, 12},
+              {201, 2, 46, 47}},
+            {{180, 140, 18, 12},
               {211, 140, 18, 12},
               {230, 140, 18, 12},
               {250, 140, 18, 12}}
