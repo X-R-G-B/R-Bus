e80cd949d829f0bd2d79760343d38239366f15cb
tenshi <tenshii.tv@gmail.com>
2023-10-03 15:29:34 +0200

    GAME-LOGIC: Add funcitonnal EntityDeathFunction MINOR

--- src/Client/Systems/Graphic/DeathSystems.cpp
+++ src/Client/Systems/Graphic/DeathSystems.cpp
@@ -6,7 +6,6 @@
 */
 
 #include "DeathSystems.hpp"
-#include <iostream>
 #include <optional>
 #include <unordered_map>
 #include "CustomTypes.hpp"
@@ -14,32 +13,23 @@
 
 namespace Systems {
 
-    // REPLACE BY YOUR DEATH FUNCTION
+    const std::function<void(std::size_t)> setPlayerAnimRectToDeath = [](std::size_t id) {
+        Registry::components<Types::AnimRect> arrAnimRect =
+            Registry::getInstance().getComponents<Types::AnimRect>();
 
-    const std::function<void(std::size_t)> testFuncPlayer = [](std::size_t id) {
-        std::cout << "Player " << id << " is dead" << std::endl;
+        if (arrAnimRect.exist(id)) {
+            Types::AnimRect &anim = arrAnimRect[id];
+            if (anim.currentRectList != Types::RectListType::DEAD) {
+                anim.changeRectList(Types::RectListType::DEAD);
+            }
+        }
     };
 
-    // MAP FOR DEATH FUNCTIONS
+    // MAP FOR DEATH FUNCTIONS FOR EACH ENTITY
     const std::unordered_map<std::type_index, std::function<void(std::size_t)>>
         deathFunctions = {
-            {std::type_index(typeid(Types::Player)), testFuncPlayer},
+            {std::type_index(typeid(Types::Player)), setPlayerAnimRectToDeath},
     };
-
-    static void addDeathFunction(
-        Registry::components<Types::Dead> &arrDead,
-        Registry::components<Types::Player> &arrPlayer,
-        std::size_t id)
-    {
-        try {
-            if (arrPlayer.exist(id)) {
-                arrDead[id].deathFunction =
-                    deathFunctions.at(std::type_index(typeid(Types::Player)));
-            }
-        } catch (std::out_of_range &) {
-            std::cerr << "No death function for this entity" << std::endl;
-        }
-    }
 
     void DeathSystems::setEntityDeathFunction(
         std::size_t /*unused*/,
@@ -47,14 +37,16 @@ namespace Systems {
     {
         Registry::components<Types::Dead> arrDead =
             Registry::getInstance().getComponents<Types::Dead>();
-        Registry::components<Types::Player> arrPlayer =
-            Registry::getInstance().getComponents<Types::Player>();
 
         std::vector<std::size_t> ids = arrDead.getExistingsId();
 
-        for (auto itIds = ids.begin(); itIds != ids.end(); itIds++) {
-            if (arrDead[*itIds].deathFunction == std::nullopt) {
-                addDeathFunction(arrDead, arrPlayer, *itIds);
+        for (const auto& [typeIndex, function] : deathFunctions) {
+            std::vector<std::size_t> entities =
+                Registry::getInstance().getEntitiesByComponents({typeIndex});
+            for (std::size_t id : entities) {
+                if (arrDead.exist(id) && arrDead[id].deathFunction == std::nullopt) {
+                    arrDead[id].deathFunction = function;
+                }
             }
         }
     }
