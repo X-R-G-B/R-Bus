fc67bd2fde45277ddadf7b77dbaf55d8dab53450
guillaume abel <guillaume.abel@epitech.eu>
2023-10-06 15:51:48 +0200

    CLIENT-NETWORK: Add receiveLifeUpdate MINOR

--- src/Client/Systems/Network/ClientNetwork.cpp
+++ src/Client/Systems/Network/ClientNetwork.cpp
@@ -9,6 +9,13 @@ namespace Systems {
         struct msgLifeUpdate_s msg = std::any_cast<struct msgLifeUpdate_s>(any);
         Registry &registry = Registry::getInstance();
         Registry::components<struct health_s> arrHealth = registry.getComponents<struct health_s>();
-        std::vector<std::size_t> ids = registry.getEntitiesByComponents();
+        std::vector<std::size_t> ids = registry.getEntitiesByComponents({typeid(struct health_s), typeid(Types::Player)});
+
+        for (auto id : ids) {
+            struct health_s &life = arrHealth[id];
+            if (life.hp != msg.life.hp) {
+                life.hp = msg.life.hp;
+            }
+        }
     }
 }
