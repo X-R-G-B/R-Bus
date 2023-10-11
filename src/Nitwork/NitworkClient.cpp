f372516a5532d4cbd53733616fb0d99042f4e579
romain <romain.panno@epitech.eu>
2023-10-11 21:01:20 +0200

    NITWORK: Fix bind problem form client by assigning first open port for the client
    PATCH

--- src/Nitwork/NitworkClient.cpp
+++ src/Nitwork/NitworkClient.cpp
@@ -32,7 +32,7 @@ namespace Nitwork {
         _endpoint = *_resolver.resolve(boost::asio::ip::udp::v4(), ip, std::to_string(port)).begin();
         boost::asio::ip::udp::socket socket(_context);
         _socket.open(boost::asio::ip::udp::v4());
-        _socket.bind(boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port));
+        _socket.bind(boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), 0));
         if (!_socket.is_open()) {
             Registry::getInstance().getLogger().error("Socket not open");
             return false;
