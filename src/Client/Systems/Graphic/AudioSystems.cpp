cc3ea492bfe07f9cef09c65eb835231d575cf04c
Tenshi <tenshii.tv@gmail.com>
2023-10-15 17:44:39 +0200

    CLIENT: Remove music on space key

--- src/Client/Systems/Graphic/AudioSystems.cpp
+++ src/Client/Systems/Graphic/AudioSystems.cpp
@@ -43,8 +43,8 @@ namespace Systems {
         }
     }
 
-    const std::string musicPath = "assets/Audio/Musics/Title.mp3";
-    const std::string soundPath = "assets/Audio/Sounds/fire.ogg";
+    const std::string musicPath      = "assets/Audio/Musics/Title.mp3";
+    const std::string soundPathShoot = "assets/Audio/Sounds/laser.ogg";
 
     void GraphicSystems::playSoundWithKey(std::size_t /*unused*/, std::size_t /*unused*/)
     {
@@ -54,12 +54,12 @@ namespace Systems {
         Registry::components<Raylib::Sound> arrSounds = registry.getComponents<Raylib::Sound>();
 
         for (auto &music : arrMusics) {
-            if (music.getPath() == musicPath && Raylib::isKeyPressed(Raylib::KeyboardKey::KB_SPACE)) {
+            if (music.getPath() == musicPath && Raylib::isKeyPressed(Raylib::KeyboardKey::KB_M)) {
                 music.setNeedToPlay(true);
             }
         }
         for (auto &sound : arrSounds) {
-            if (sound.getPath() == soundPath && Raylib::isKeyPressed(Raylib::KeyboardKey::KB_ENTER)) {
+            if (sound.getPath() == soundPathShoot && Raylib::isKeyPressed(Raylib::KeyboardKey::KB_SPACE)) {
                 sound.setNeedToPlay(true);
             }
         }
@@ -71,11 +71,11 @@ namespace Systems {
         constexpr float soundVolume = 0.63F;
 
         Raylib::Music music(musicPath, musicVolume);
-        Raylib::Sound sound(soundPath, soundVolume);
+        Raylib::Sound soundEffectShoot(soundPathShoot, soundVolume);
 
         Registry::getInstance().addEntity();
         Registry::getInstance().getComponents<Raylib::Music>().insertBack(music);
-        Registry::getInstance().getComponents<Raylib::Sound>().insertBack(sound);
+        Registry::getInstance().getComponents<Raylib::Sound>().insertBack(soundEffectShoot);
         SystemManagersDirector::getInstance().getSystemManager(managerId).removeSystem(systemId);
     }
 
