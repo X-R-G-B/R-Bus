ab6f391081d29f66eaf7113fc2062a517b06c52e
Tenshi <tenshii.tv@gmail.com>
2023-10-20 03:39:28 +0200

    GAME-LOGIC: Separe bullet sound MINOR

--- src/ECS/Systems/BulletsSystems.cpp
+++ src/ECS/Systems/BulletsSystems.cpp
@@ -12,26 +12,46 @@
 #ifdef CLIENT
     #include "CustomTypes.hpp"
     #include "NitworkClient.hpp"
+    #include "Raylib.hpp"
 #else
     #include "NitworkServer.hpp"
 #endif
 
 namespace Systems {
 
+#ifdef CLIENT
+    static void playBulletSound(Types::Missiles &typeOfMissile)
+    {
+        Json &json = Json::getInstance();
+        Registry::components<Raylib::Sound> arrSounds = Registry::getInstance().getComponents<Raylib::Sound>();
+        nlohmann::json bulletData = json.getJsonObjectById(JsonType::BULLETS, "default");
+
+        const std::string soundPathShoot =
+            json.getDataFromJson<std::string>(bulletData, "soundPath");
+
+        for (auto &sound : arrSounds) {
+            if (sound.getPath() == soundPathShoot) {
+                sound.setNeedToPlay(true);
+                break;
+            }
+        }
+    }
+#endif
+
     void createMissile(Types::Position &pos, Types::Missiles &typeOfMissile)
     {
         Json &json = Json::getInstance();
         Registry::getInstance().addEntity();
         nlohmann::json bulletData = json.getJsonObjectById(JsonType::BULLETS, "default");
-
-        Types::CollisionRect collisionRect = json.getDataFromJson<Types::CollisionRect>(bulletData, "collisionRect");
-        Types::Velocity velocity           = json.getDataFromJson<Types::Velocity>(bulletData, "velocity");
-        Types::Missiles missileType          = typeOfMissile;
-        Types::Dead deadComp                 = {};
+        Types::CollisionRect collisionRect =
+            json.getDataFromJson<Types::CollisionRect>(bulletData, "collisionRect");
+        Types::Velocity velocity    = json.getDataFromJson<Types::Velocity>(bulletData, "velocity");
+        Types::Missiles missileType = typeOfMissile;
+        Types::Dead deadComp        = {};
         Types::PlayerAllies playerAlliesComp = {};
         Types::Position position             = {pos.x, pos.y};
-        struct health_s healthComp = {1};
-        Types::Damage damageComp   = {10};
+        struct health_s healthComp           = {1};
+        Types::Damage damageComp             = {10};
 
 #ifdef CLIENT
         const std::string bulletPath = json.getDataFromJson<std::string>(bulletData, "spritePath");
@@ -44,7 +64,9 @@ namespace Systems {
             static_cast<std::size_t>(FRONT));
         Registry::getInstance().getComponents<Types::SpriteDatas>().insertBack(bulletDatas);
         Registry::getInstance().getComponents<Types::Rect>().insertBack(spriteRect);
+        playBulletSound(typeOfMissile);
 #endif
+
         Registry::getInstance().getComponents<Types::Position>().insertBack(position);
         Registry::getInstance().getComponents<Types::CollisionRect>().insertBack(collisionRect);
         Registry::getInstance().getComponents<Types::Missiles>().insertBack(missileType);
