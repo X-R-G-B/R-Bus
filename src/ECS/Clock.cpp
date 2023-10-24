b8faffbd40b6c976c6bbc1d44890727b017ed68f
Xavier Mitault <xavier.mitault@epitech.eu>
2023-10-24 10:06:35 +0200

    ECS: Fix clock resolution on platform

--- src/ECS/Clock.cpp
+++ src/ECS/Clock.cpp
@@ -7,7 +7,7 @@ std::size_t Clock::create(bool deferStart)
     if (deferStart) {
         _clocks.emplace_back(std::nullopt);
     } else {
-        _clocks.emplace_back(std::chrono::high_resolution_clock::now());
+        _clocks.emplace_back(std::chrono::system_clock::now());
     }
     return _clocks.size() - 1;
 }
@@ -17,7 +17,7 @@ std::size_t Clock::elapsedMillisecondsSince(std::size_t id)
     if (!_clocks[id].has_value()) {
         return static_cast<std::size_t>(-1);
     }
-    auto val = std::chrono::high_resolution_clock::now() - _clocks[id].value();
+    auto val = std::chrono::system_clock::now() - _clocks[id].value();
     return std::chrono::duration_cast<std::chrono::milliseconds>(val).count();
 }
 
@@ -26,7 +26,7 @@ std::size_t Clock::elapsedSecondsSince(std::size_t id)
     if (!_clocks[id].has_value()) {
         return static_cast<std::size_t>(-1);
     }
-    auto val = std::chrono::high_resolution_clock::now() - _clocks[id].value();
+    auto val = std::chrono::system_clock::now() - _clocks[id].value();
     return std::chrono::duration_cast<std::chrono::seconds>(val).count();
 }
 
@@ -35,7 +35,7 @@ std::size_t Clock::elapsedNanosecondsSince(std::size_t id)
     if (!_clocks[id].has_value()) {
         return static_cast<std::size_t>(-1);
     }
-    auto val = std::chrono::high_resolution_clock::now() - _clocks[id].value();
+    auto val = std::chrono::system_clock::now() - _clocks[id].value();
     return std::chrono::duration_cast<std::chrono::nanoseconds>(val).count();
 }
 
@@ -65,10 +65,10 @@ void Clock::decreaseNanoseconds(std::size_t id, std::size_t nanoseconds)
 
 void Clock::restart(std::size_t id)
 {
-    _clocks[id] = std::chrono::high_resolution_clock::now();
+    _clocks[id] = std::chrono::system_clock::now();
 }
 
 void Clock::restart(std::size_t id, std::time_t time)
 {
-    _clocks[id] = std::chrono::high_resolution_clock::from_time_t(time);
+    _clocks[id] = std::chrono::system_clock::from_time_t(time);
 }
