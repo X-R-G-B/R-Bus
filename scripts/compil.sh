375a75a4622139b5bb7ef90dea6c1eca013cec5b
Xavier Mitault <xavier.mitault@epitech.eu>
2023-10-30 08:59:24 +0000

    CICD: remove parallel
    PATCH

--- scripts/compil.sh
+++ scripts/compil.sh
@@ -3,10 +3,14 @@
 
 set -ex
 
-if [[ "$1" == "--no-tidy" ]]; then
+if [[ "$1" == "--no-tidy" ]] || [[ "$2" == "--no-tidy" ]]; then
     cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DNO_USE_CLANG_TIDY=true
 else
     cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug
 fi
 
-cmake --build build --parallel
+if [[ "$1" == "--no-parallel" ]] || [[ "$2" == "--no-parallel" ]]; then
+    cmake --build build
+else
+    cmake --build build --parallel
+fi
